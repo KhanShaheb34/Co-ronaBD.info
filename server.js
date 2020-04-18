26f1c2bbbd2d0656527a4815673de3afe581b3af
KhanShaheb34 <shakirul34@student.sust.edu>
2020-04-18 22:21:10 +0600

    Fix production problems

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const app = express();
 app.use(favicon(__dirname + "/build/favicon.ico"));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function (req, res) {
-  return res.send("pong");
-});
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
