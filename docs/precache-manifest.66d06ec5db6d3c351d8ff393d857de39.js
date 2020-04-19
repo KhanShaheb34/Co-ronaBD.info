60ff0ade4f5652eee1df4b8988e3e84a9d898f1d
KhanShaheb34 <shakirul34@student.sust.edu>
2020-04-20 00:54:04 +0600

    github pages

---
+++ docs/precache-manifest.66d06ec5db6d3c351d8ff393d857de39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79c82d5b7341ce3212d85095190f505",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15fad1e77103bd772007",
+    "url": "/static/css/2.0a774ccf.chunk.css"
+  },
+  {
+    "revision": "c2e92b2c16d57c2f6535",
+    "url": "/static/css/main.072720d4.chunk.css"
+  },
+  {
+    "revision": "15fad1e77103bd772007",
+    "url": "/static/js/2.ac2a8b94.chunk.js"
+  },
+  {
+    "revision": "6124e4358eb44bd94caab059bc72baa3",
+    "url": "/static/js/2.ac2a8b94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2e92b2c16d57c2f6535",
+    "url": "/static/js/main.e1e02b43.chunk.js"
+  },
+  {
+    "revision": "8098ec04d0294bbecc8c",
+    "url": "/static/js/runtime-main.54b4cc03.js"
+  }
+]);
