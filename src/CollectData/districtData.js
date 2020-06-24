f1ab1f79613e45c6a8e8d19e51352d7128cc4696
KhanShaheb34 <shakirul34@student.sust.edu>
2020-06-25 00:11:58 +0600

    Fixed district data name mapping, fixed #7

--- src/CollectData/districtData.js
+++ src/CollectData/districtData.js
@@ -3,10 +3,9 @@ import axios from "axios";
 const url = "https://corona-bd.herokuapp.com/district";
 
 const nameMap = [
-  { from: "Dhaka City", to: "Dhaka" },
+  { from: "Dhaka (District)", to: "Dhaka" },
   { from: "Barishal", to: "Barisal" },
   { from: "Jhalokathi", to: "Jhalokati" },
-  { from: "Potuakhali", to: "Patuakhali" },
   { from: "Chattogram", to: "Chittagong" },
   { from: "Cumilla", to: "Comilla" },
   { from: "Cox\u2019s bazar", to: "Cox'S Bazar" },
@@ -21,15 +20,22 @@ const nameMap = [
   { from: "Chapainawabganj", to: "Nawabganj" },
   { from: "Khagrachari", to: "Khagrachhari" },
   { from: "Rangmati", to: "Rangamati" },
+  { from: "Bogura", to: "Bogra" },
 ];
 
 export default async () => {
   let res = await axios.get(url);
 
   nameMap.map((name) => {
-    return (res.data.data[
-      res.data.data.findIndex((dist) => dist.name === name.from)
-    ].name = name.to);
+    if (
+      res.data.data[res.data.data.findIndex((dist) => dist.name === name.from)]
+    ) {
+      return (res.data.data[
+        res.data.data.findIndex((dist) => dist.name === name.from)
+      ].name = name.to);
+    } else {
+      return name;
+    }
   });
 
   return res.data;
