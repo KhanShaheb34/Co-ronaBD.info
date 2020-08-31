b1d66d7ed2641cf4f593153a36340a038df1c7f2
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-08-31 11:52:34 +0600

    implement caching for server response

--- src/CollectData/data.js
+++ src/CollectData/data.js
@@ -1,12 +1,15 @@
 import overallData from './overallData';
 import districtData from './districtData';
 
+export const districtCacheKey = 'coronainfo:district';
+export const statCacheKey = 'coronainfo:stats';
+
 export default async () => {
-  const overall = await overallData();
-  const district = await districtData();
-  return {
-    ...overall,
-    districts: district.data,
-    district_data_updated_on: district.updated_on,
-  };
+	const overall = await overallData();
+	const district = await districtData();
+	return {
+		...overall,
+		districts: district.data,
+		district_data_updated_on: district.updated_on,
+	};
 };
--- src/CollectData/districtData.js
+++ src/CollectData/districtData.js
@@ -1,42 +1,53 @@
 import axios from 'axios';
+import { districtCacheKey } from './data';
 
 const url = 'https://corona-bd.herokuapp.com/district';
 
 const nameMap = [
-  { from: 'Dhaka City', to: 'Dhaka' },
-  { from: 'Barishal', to: 'Barisal' },
-  { from: 'Jhalokathi', to: 'Jhalokati' },
-  { from: 'Chattogram', to: 'Chittagong' },
-  { from: 'Cumilla', to: 'Comilla' },
-  { from: 'Cox\u2019s bazar', to: "Cox'S Bazar" },
-  { from: 'Laksmipur', to: 'Lakshmipur' },
-  { from: 'Munshigonj', to: 'Munshiganj' },
-  { from: 'Netrokona', to: 'Netrakona' },
-  { from: 'Hobiganj', to: 'Habiganj' },
-  { from: 'Moulovi Bazar', to: 'Moulvibazar' },
-  { from: 'B. Baria', to: 'Brahamanbaria' },
-  { from: 'Narshingdi', to: 'Narsingdi' },
-  { from: 'Panchagar', to: 'Panchagarh' },
-  { from: 'Chapainawabganj', to: 'Nawabganj' },
-  { from: 'Khagrachari', to: 'Khagrachhari' },
-  { from: 'Rangmati', to: 'Rangamati' },
-  { from: 'Bogura', to: 'Bogra' },
+	{ from: 'Dhaka City', to: 'Dhaka' },
+	{ from: 'Barishal', to: 'Barisal' },
+	{ from: 'Jhalokathi', to: 'Jhalokati' },
+	{ from: 'Chattogram', to: 'Chittagong' },
+	{ from: 'Cumilla', to: 'Comilla' },
+	{ from: 'Cox\u2019s bazar', to: "Cox'S Bazar" },
+	{ from: 'Laksmipur', to: 'Lakshmipur' },
+	{ from: 'Munshigonj', to: 'Munshiganj' },
+	{ from: 'Netrokona', to: 'Netrakona' },
+	{ from: 'Hobiganj', to: 'Habiganj' },
+	{ from: 'Moulovi Bazar', to: 'Moulvibazar' },
+	{ from: 'B. Baria', to: 'Brahamanbaria' },
+	{ from: 'Narshingdi', to: 'Narsingdi' },
+	{ from: 'Panchagar', to: 'Panchagarh' },
+	{ from: 'Chapainawabganj', to: 'Nawabganj' },
+	{ from: 'Khagrachari', to: 'Khagrachhari' },
+	{ from: 'Rangmati', to: 'Rangamati' },
+	{ from: 'Bogura', to: 'Bogra' },
 ];
 
 export default async () => {
-  let res = await axios.get(url);
+	let res;
+	try {
+		res = await axios.get(url);
+		localStorage.setItem(districtCacheKey, JSON.stringify(res));
+	} catch (e) {
+		// if response fails use cached data
+		res = localStorage.getItem(districtCacheKey);
+		res = JSON.parse(res);
+	}
 
-  nameMap.map((name) => {
-    if (
-      res.data.data[res.data.data.findIndex((dist) => dist.name === name.from)]
-    ) {
-      return (res.data.data[
-        res.data.data.findIndex((dist) => dist.name === name.from)
-      ].name = name.to);
-    } else {
-      return name;
-    }
-  });
+	nameMap.map((name) => {
+		if (
+			res.data.data[
+				res.data.data.findIndex((dist) => dist.name === name.from)
+			]
+		) {
+			return (res.data.data[
+				res.data.data.findIndex((dist) => dist.name === name.from)
+			].name = name.to);
+		} else {
+			return name;
+		}
+	});
 
-  return res.data;
+	return res.data;
 };
